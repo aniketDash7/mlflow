05477bbd2f411f2db35ce10e06a1ff18e7813d55
aniketDash7 <aniket.addash@gmail.com>
2024-07-31 00:15:27 +0530

    Data Validation added

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from mlflow import logger 
 
 from mlflow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlflow.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+
 STAGE_NAME = "Data Ingestion Stage"
 try:
-    logger.info(f"{STAGE_NAME} started ")
+    logger.info(f"{STAGE_NAME} started")
     obj = DataIngestionTrainingPipeline()
     obj.main()
-    logger.info(f"{STAGE_NAME} completed")
+    logger.info(f"{STAGE_NAME} completed.")
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f"{STAGE_NAME} started")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f"{STAGE_NAME} completed.")
+except Exception as e:
+    logger.exception(e)
+    raise(e)
