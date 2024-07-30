93c6f59fa6d3cbd20583aebe7489deda926c6dfc
aniketDash7 <aniket.addash@gmail.com>
2024-07-30 12:07:25 +0530

    Data Ingestion Pipeline completed

---
+++ main.py
@@ -0,0 +1,12 @@
+from mlflow import logger 
+
+from mlflow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f"{STAGE_NAME} started ")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f"{STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
